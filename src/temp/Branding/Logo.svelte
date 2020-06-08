f690a7d70680589698812f6375b884099b820b0f
kevmodrome <kevin@kevmodro.me>
2020-06-08 13:31:45 +0200

    semi-fix path issues

---
+++ src/temp/Branding/Logo.svelte
@@ -0,0 +1,16 @@
+<script>
+  import BudibaseLogo from "./Budibase-logo.svelte";
+</script>
+
+<style>
+  .home-logo {
+    cursor: pointer;
+    height: 40px;
+    width: 100px;
+    margin-bottom: 20px;
+  }
+</style>
+
+<div class="home-logo">
+  <BudibaseLogo />
+</div>
