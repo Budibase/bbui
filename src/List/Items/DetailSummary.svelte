33d6e9a050f6c93b7be48a1b4e6290ae564f3258
Kevin Åberg Kultalahti <kevmodrome@users.noreply.github.com>
2020-06-09 10:01:18 +0200

    Feature/add detailsummary (#6)
    * feat: add detailsummary component
    
    * export component

---
+++ src/List/Items/DetailSummary.svelte
@@ -0,0 +1,68 @@
+<script>
+  // your script goes here
+  export let name = "Name",
+    icon,
+    show;
+
+  const capitalize = name => name[0].toUpperCase() + name.slice(1);
+</script>
+
+<style>
+  .property-group-container {
+    display: flex;
+    flex-direction: column;
+    height: auto;
+    background: var(--grey-light);
+    margin: 0px 0px 4px 0px;
+    padding: 8px 12px;
+    justify-content: center;
+    border-radius: 4px;
+  }
+
+  .property-group-name {
+    cursor: pointer;
+    display: flex;
+    flex-flow: row nowrap;
+  }
+
+  .name {
+    flex: 1;
+    text-align: left;
+    padding-top: 2px;
+    font-size: 14px;
+    font-weight: 500;
+    letter-spacing: 0.14px;
+    color: var(--ink);
+  }
+
+  .icon {
+    flex: 0 0 20px;
+    text-align: center;
+  }
+
+  .property-panel {
+    /* height: 0px;
+    overflow: hidden; */
+    display: none;
+  }
+
+  .show {
+    /* overflow: auto;
+    height: auto; */
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+  }
+</style>
+
+<div class="property-group-container">
+  <div class="property-group-name" on:click={() => (show = !show)}>
+    <div class="icon">
+      <i class={icon} />
+    </div>
+    <div class="name">{capitalize(name)}</div>
+  </div>
+  <div class="property-panel" class:show>
+    <slot />
+  </div>
+</div>
