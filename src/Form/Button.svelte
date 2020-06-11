851afcbf9b9ea35c09e61171601dc3070f20b041
Joe <49767913+joebudi@users.noreply.github.com>
2020-06-11 12:44:48 +0100

    feat: Button update (#12)
    * Feat: Updated button UI
    
    New buttons, including attention, warning added, as well as new sizes.
    
    * Yarn.lock update to potentially fix npm tree error

--- src/Form/Button.svelte
+++ src/Form/Button.svelte
@@ -1,51 +1,130 @@
 <script>
-  export let primary = false,
-    outline = false,
+  export let 
+    primary = false,
+    secondary = false,
+    attention = false,
     disabled = false,
     error = false,
-    thin = false;
+    warning = false,
+    small = false,
+    medium = false,
+    wide = false,
+    large = false;
 </script>
 
 <style>
   button {
     cursor: pointer;
-    font-weight: bold;
-    font-size: 18px;
+    font-weight: 500;
+    font-size: 14px;
+    box-sizing: border-box;
+    overflow: hidden;
     border-radius: 3px;
-    padding: 20px 30px;
-    color: white;
-  }
-  button:hover:not([disabled]) {
-    filter: brightness(85%);
+    color: var(--white);
+    padding: 0px 24px;
+    height: 32px;
+    transition: all 0.2s ease 0s;
+    display: inline-block;
+    text-rendering: optimizeLegibility;
+    -webkit-font-smoothing: antialiased;
+    min-width: auto;
+    outline: none;
+    font-feature-settings: "case" 1,"rlig" 1,"calt" 0;
+    -webkit-box-align: center;
+    user-select: none;
+    flex-shrink: 0;
   }
 
   .primary {
-    border: solid 0 var(--primary100);
-    background-color: var(--primary100);
+    border: solid 1px var(--ink);
+    background-color: var(--ink);
   }
-  .outline {
-    border: solid 1px var(--grey-medium);
+  button.primary:hover:not([disabled]) {
+    border: solid 1px var(--grey);
+    background-color: var(--white);
+    color: var(--ink);
+  }
+  .secondary {
+    border: solid 1px var(--grey);
     background-color: white;
-    color: var(--font);
+    color: var(--ink);
+  }
+  button.secondary:hover:not([disabled]) {
+    border: solid 1px var(--ink);
+    background-color: var(--ink);
+    color: white;
+  }
+  .attention {
+    background-color: var(--blue);
+    border: solid 1px var(--blue);
+  }
+  button.attention:hover:not([disabled]) {
+    border: solid 1px var(--blue);
+    background-color: var(--white);
+    color: var(--blue);
   }
   .error {
-    border: solid 0 var(--error100);
-    background-color: var(--error100);
+    border: solid 1px var(--red);
+    background-color: var(--red);
+    color: white;
   }
-  .thin {
-    padding: 12px 16px;
+  button.error:hover:not([disabled]) {
+    border: solid 1px var(--red);
+    background-color: white;
+    color: var(--red);
+  }
+  .warning {
+    border: solid 1px var(--yellow);
+    background-color: var(--yellow);
+    color: white;
+  }
+  button.warning:hover:not([disabled]) {
+    border: solid 1px var(--yellow);
+    background-color: white;
+    color: var(--yellow);
+  }
+  .small {
+    width: 100px;
+    height: 24px;
+    line-height: 22px;
+    padding: 0px 10px;
+    font-weight: 500;
+  }
+  .medium {
+    width: 200px;
+    height: 40px;
+  }
+  .large {
+    width: 200px;
+    height: 50px;
+    font-weight: 500;
+    font-size: 18px;
+  }
+  .wide {
+    width: 100%;
+    max-width: 260px;
+    height: 32px;
+    line-height: 22px;
+    padding: 0px 10px;
+    font-weight: 500;
   }
   button:disabled {
-    background-color: #adaec4;
+    background-color: var(--grey-dark);
     cursor: not-allowed;
+    border: solid 1px var(--grey-dark);
   }
 </style>
 
 <button
   class:primary
-  class:outline
+  class:secondary
+  class:attention
   class:error
-  class:thin
+  class:warning
+  class:small
+  class:medium
+  class:wide
+  class:large
   {disabled}
   on:click|preventDefault>
   <slot />
