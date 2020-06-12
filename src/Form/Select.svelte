0e36745cb2e1d3860c9ad417d81fa883cb7c7246
Martin McKeaveney <martinmckeaveney@gmail.com>
2020-06-12 14:17:39 +0100

    Revert "Select element and ListItem Element (#14)"
    This reverts commit 18922716ed1e08979fe298d67bb665507b7c3fe8.

--- src/Form/Select.svelte
+++ src/Form/Select.svelte
@@ -1,26 +1,50 @@
 <script>
-  export let value;
-  export let primary;
-  export let secondary;
+  export let name,
+    label,
+    type,
+    placeholder,
+    validator = () => {};
 </script>
 
 <style>
-  select {
-    height: 35px;
-    width: 220px;
-    border-radius: 3px;
-    border: 1px solid var(--grey-dark);
-    text-align: left;
-    color: var(--ink);
-    font-size: 14px;
-    padding-left: 12px;
+  .container {
+    display: flex;
+    flex-direction: column;
+    font-size: 18px;
+    font-weight: 500;
+    line-height: 1.17;
   }
 
-  .secondary {
-    background: var(--grey);
+  label {
+    color: var(--font);
+    margin-bottom: 12px;
+  }
+
+  input {
+    box-sizing: border-box;
+    color: var(--font);
+    font-size: 18px;
+    border-radius: 3px;
+    border: solid 1px var(--grey-medium);
+    padding: 20px;
+    margin: 0;
+    background-color: white;
+  }
+
+  input::placeholder {
+    color: var(--grey-medium);
+  }
+
+  input:focus {
+    box-shadow: 0 4px 16px 0 rgba(57, 60, 68, 0.08);
+  }
+
+  input:disabled {
+    background: var(--grey-medium);
   }
 </style>
 
-<select class:secondary on:change bind:value>
-  <slot />
-</select>
+<div class="container">
+  <label for={name}>{label}</label>
+  <input on:change on:input use:validator {type} {name} {placeholder} />
+</div>
