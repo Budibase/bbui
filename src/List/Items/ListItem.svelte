8c57374a6b82fc10aa8014013461d20ca393f607
Martin McKeaveney <martinmckeaveney@gmail.com>
2020-06-12 14:21:15 +0100

    Revert "Revert "Select element and ListItem Element (#14)" (#15)"
    This reverts commit 5a7b27c2a8b0d4d7ca550ac01e74400e9589c4a6.

---
+++ src/List/Items/ListItem.svelte
@@ -0,0 +1,35 @@
+<script>
+  export let icon
+  export let title
+</script>
+
+<style>
+  div {
+    width: 260px;
+    height: 40px;
+    border-radius: 3px;
+    font-weight: bold;
+    display: flex;
+    align-items: center;
+    transition: 0.3s background-color;
+    color: var(--ink);
+    font-size: 14px;
+  }
+
+  div:hover {
+    background-color: var(--secondary);
+    cursor: pointer;
+  }
+
+  i {
+    width: 18px;
+    height: 18px;
+    background-color: var(--dark-grey);
+    margin-right: 8px;
+  }
+</style>
+
+<div on:click>
+  <i class={icon} />
+  <span>{title}</span>
+</div>
