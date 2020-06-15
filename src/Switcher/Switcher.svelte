8929eb3f6232f666a8ecdf446c9d00c032341838
Martin McKeaveney <martin@shogunsystems.co.uk>
2020-06-15 11:33:25 +0100

    feat: adding header switcher component

---
+++ src/Switcher/Switcher.svelte
@@ -0,0 +1,39 @@
+<script>
+  export let headings;
+  export let value;
+</script>
+
+<style>
+  header {
+    display: grid;
+    grid-gap: 20px;
+    grid-auto-flow: column;
+  }
+
+  span {
+    font-size: 18px;
+    font-weight: bold;
+    color: var(--ink-lighter);
+  }
+
+  span:hover {
+    cursor: pointer;
+  }
+
+  .selected {
+    color: var(--ink);
+  }
+</style>
+
+<section>
+  <header>
+    {#each headings as heading}
+      <span
+        class:selected={value === heading.key}
+        on:click={() => (value = heading.key)}>
+        {heading.title}
+      </span>
+    {/each}
+  </header>
+  <slot />
+</section>
