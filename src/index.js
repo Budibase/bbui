3384639b717cd0df7bb973a17273b24dd12dd12e
kevmodrome <kevin@kevmodro.me>
2020-08-05 16:54:07 +0200

    fix: change imports

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ export { default as Input } from './Form/Input.svelte';
 export { default as TextArea } from './Form/TextArea.svelte';
 export { default as Select } from './Form/Select.svelte';
 export { default as Button } from './Button/Button.svelte';
-export { default as Icon, iconOptions, directions } from './Icons/Icon.svelte';
+export { default as Icon, iconOptions, directions } from './TempIcons/Icon.svelte';
 export { default as TextButton } from './Button/TextButton.svelte';
 export { default as Home } from './Links/Home.svelte';
 export { default as DetailSummary } from './List/Items/DetailSummary.svelte';
