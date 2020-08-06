6a7592ef2dbcf6443d3a07827b003af0752efe72
Victoria Sloan <victoriasloan@gmail.com>
2020-08-06 22:28:46 +0100

    typography component created

---
+++ src/Styleguide/Typography.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let type;
+  export let color;
+  export let size;
+  export let fontWeight;
+
+  const classname = `bb-${type} bb-${type}--${size} bb-${type}--${fontWeight} bb-${type}--color-${color}`;
+</script>
+
+{#if type === 'heading'}
+  <h1 class={classname}>
+    <slot />
+  </h1>
+{/if}
+
+{#if type === 'body'}
+  <p class={classname} />
+{/if}
