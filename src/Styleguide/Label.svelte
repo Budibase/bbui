31625a273a03ca040f8feff3c292426667f35bef
Victoria Sloan <victoriasloan@gmail.com>
2020-08-16 18:58:32 +0100

    label svelte file

---
+++ src/Styleguide/Label.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let color = "dark";
+  export let size = "m";
+  export let forAttr = " ";
+
+  const classname = `bb-label bb-label--${size} bb-label--color-${color}`;
+</script>
+
+<label class={classname} for={forAttr}>
+  <slot />
+</label>
