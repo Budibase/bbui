4b797b04d55e4feb92ab041306a0184787a14744
Tori Sloan <victoriasloan95@gmail.com>
2020-08-22 19:18:52 +0100

    feat: typography component created (#80)
    * text component renamed to typography and container containing all options before any styles applied
    
    * typography component created
    
    * body using typography component, slot added
    
    * default prop values set
    
    * fix large typography color light
    
    * heading and body typography components created
    
    * xs and xl sizes created for heading and typography, redundant css rules removed
    
    * fontWeight prop removed from typography components
    
    * label typography component created
    
    * label svelte file
    
    Co-authored-by: Victoria Sloan <victoriasloan@gmail.com>

---
+++ src/Styleguide/Body.svelte
@@ -0,0 +1,10 @@
+<script>
+  export let color = "dark";
+  export let size = "m";
+
+  const classname = `bb-body bb-body--${size} bb-body--color-${color}`;
+</script>
+
+<p class={classname}>
+  <slot />
+</p>
---
+++ src/Styleguide/Heading.svelte
@@ -0,0 +1,10 @@
+<script>
+  export let color = "dark";
+  export let size = "m";
+
+  const classname = `bb-heading bb-heading--${size} bb-heading--color-${color}`;
+</script>
+
+<h1 class={classname}>
+  <slot />
+</h1>
