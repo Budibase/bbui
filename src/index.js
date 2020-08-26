d400452daeae0804faf69b6d42d61d4bfaa29ecf
Conor_Mack <36074859+Conor-Mack@users.noreply.github.com>
2020-08-26 15:54:43 +0100

    fix: export typography components (#100)

--- src/index.js
+++ src/index.js
@@ -7,15 +7,17 @@
 //     import { Foo, Bar } from 'my-lib'
 //
 
-export { default as Input } from './Form/Input.svelte';
-export { default as TextArea } from './Form/TextArea.svelte';
-export { default as Select } from './Form/Select.svelte';
-export { default as Button } from './Button/Button.svelte';
-export { default as Icon, iconOptions, directions } from './Icons/Icon.svelte';
-export { default as TextButton } from './Button/TextButton.svelte';
-export { default as Home } from './Links/Home.svelte';
-export { default as DetailSummary } from './List/Items/DetailSummary.svelte';
+export { default as Input } from "./Form/Input.svelte";
+export { default as TextArea } from "./Form/TextArea.svelte";
+export { default as Select } from "./Form/Select.svelte";
+export { default as Button } from "./Button/Button.svelte";
+export { default as Icon, iconOptions, directions } from "./Icons/Icon.svelte";
+export { default as TextButton } from "./Button/TextButton.svelte";
+export { default as Home } from "./Links/Home.svelte";
+export { default as DetailSummary } from "./List/Items/DetailSummary.svelte";
 export { default as Switcher } from "./Switcher/Switcher.svelte";
 export { default as DropdownMenu } from "./DropdownMenu/DropdownMenu.svelte";
 export { default as Popover } from "./DropdownMenu/Popover.svelte";
-
+export { default as Body } from "./Styleguide/Body.svelte";
+export { default as Heading } from "./Styleguide/Heading.svelte";
+export { default as Label } from "./Styleguide/Label.svelte";
