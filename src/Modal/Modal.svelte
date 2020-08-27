5b4dcd5d98f59be1ff5b9c037617d9e4036fc8e2
Michael Shanks <mike@budibase.com>
2020-08-27 16:42:57 +0100

    fix: Modal - same style, more usable API

--- src/Modal/Modal.svelte
+++ src/Modal/Modal.svelte
@@ -1,158 +1,73 @@
 <script>
-  import { setContext as baseSetContext, SvelteComponent } from "svelte";
-  import { fade } from "svelte/transition";
+  import { createEventDispatcher } from "svelte";
+  import buildStyle from "../utils/buildStyle"
+  import { fade } from 'svelte/transition';
 
-  export let key = "simple-modal";
-  export let closeButton = true;
-  export let closeOnEsc = true;
-  export let closeOnOuterClick = true;
-  export let styleBg = { top: 0, left: 0 };
-  export let styleWindow = {};
-  export let styleContent = {};
-  export let styleCloseButton = {};
-  export let setContext = baseSetContext;
-  export let transitionBg = fade;
-  export let transitionBgProps = { duration: 250 };
-  export let transitionWindow = transitionBg;
-  export let transitionWindowProps = transitionBgProps;
+  const dispatch = createEventDispatcher();
+  export let maxWidth = "30vw";
+  export let maxHeight = "60vh";
+  export let borderColor = "";
 
-  const defaultState = {
-    closeButton,
-    closeOnEsc,
-    closeOnOuterClick,
-    styleBg,
-    styleWindow,
-    styleContent,
-    styleCloseButton,
-    transitionBg,
-    transitionBgProps,
-    transitionWindow,
-    transitionWindowProps
-  };
-  let state = { ...defaultState };
-
-  let Component = null;
-  let props = null;
-
-  let background;
-  let wrap;
-  let modalWindow;
-
-  const camelCaseToDash = str =>
-    str.replace(/([a-zA-Z])(?=[A-Z])/g, "$1-").toLowerCase();
-
-  const toCssString = props =>
-    Object.keys(props).reduce(
-      (str, key) => `${str}; ${camelCaseToDash(key)}: ${props[key]}`,
-      ""
-    );
-
-  const isSvelteComponent = component =>
-    SvelteComponent.isPrototypeOf(component);
-
-  $: cssBg = toCssString(state.styleBg);
-  $: cssWindow = toCssString(state.styleWindow);
-  $: cssContent = toCssString(state.styleContent);
-  $: cssCloseButton = toCssString(state.styleCloseButton);
-  $: currentTransitionBg = state.transitionBg;
-  $: currentTransitionWindow = state.transitionWindow;
-
-  const toVoid = () => {};
-  let onOpen = toVoid;
-  let onClose = toVoid;
-  let onOpened = toVoid;
-  let onClosed = toVoid;
-
-  const open = (NewComponent, newProps = {}, options = {}, callback = {}) => {
-    Component = NewComponent;
-    props = newProps;
-    state = { ...defaultState, ...options };
-    onOpen = callback.onOpen || toVoid;
-    onClose = callback.onClose || toVoid;
-    onOpened = callback.onOpened || toVoid;
-    onClosed = callback.onClosed || toVoid;
+  export const show = () => {
+    dispatch("open");
+    open = true;
   };
 
-  const close = (callback = {}) => {
-    onClose = callback.onClose || onClose;
-    onClosed = callback.onClosed || onClosed;
-    Component = null;
-    props = null;
+  export const hide = () => {
+    dispatch("close");
+    open = false;
   };
 
-  const handleKeydown = event => {
-    if (state.closeOnEsc && Component && event.key === "Escape") {
-      event.preventDefault();
-      close();
+  let open = null;
+
+  function handleEscape(e) {
+    if (open && e.key === "Escape") {
+      hide();
     }
+  }
 
-    if (Component && event.key === "Tab") {
-      // trap focus
-      const nodes = modalWindow.querySelectorAll("*");
-      const tabbable = Array.from(nodes).filter(node => node.tabIndex >= 0);
-
-      let index = tabbable.indexOf(document.activeElement);
-      if (index === -1 && event.shiftKey) index = 0;
-
-      index += tabbable.length + (event.shiftKey ? -1 : 1);
-      index %= tabbable.length;
-
-      tabbable[index].focus();
-      event.preventDefault();
-    }
-  };
-
-  const handleOuterClick = event => {
-    if (
-      state.closeOnOuterClick &&
-      (event.target === background || event.target === wrap)
-    ) {
-      event.preventDefault();
-      close();
-    }
-  };
-
-  setContext(key, { open, close });
+  $: containerStyle = buildStyle({
+    "max-height": maxHeight,
+    "max-width": maxWidth,
+    borderColor,
+  });
 </script>
 
 <style>
-  * {
-    box-sizing: border-box;
-  }
-
-  .bg {
+  .overlay {
     position: fixed;
-    z-index: 1000;
     display: flex;
-    flex-direction: column;
     justify-content: center;
-    width: 100vw;
-    height: 100vh;
+    align-items: center;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    z-index: 1;
     background: rgba(0, 0, 0, 0.6);
   }
 
-  .window-wrap {
-    position: relative;
-    margin: 2rem;
-    max-height: 100%;
-  }
-
-  .window {
-    position: relative;
+  .container {
+    outline: none;
+    box-sizing: border-box;
+    opacity: 0;
     width: 400px;
-    max-width: 100%;
-    max-height: 100%;
-    margin: 2rem auto;
-    color: black;
+    min-width: 200px;
+    min-height: 200px;
+    color: var(--ink);
+    font-weight: 400;
+    padding: var(--layout-s);
+    height: fit-content !important;
+    border: black;
     border-radius: var(--border-radius-m);
-    background: white;
+    overflow-y: auto;
+    background: #fff;
+    box-shadow: 0 5px 12px rgba(0, 0, 0, 0.15);
   }
 
-  .content {
-    padding: var(--layout-s);
-    position: relative;
-    max-height: calc(100vh - 4rem);
-    overflow: auto;
+  .open {
+    transform: scale(1);
+    opacity: 1;
   }
 
   .close {
@@ -186,43 +101,21 @@
     background-color: var(--grey-4);
     cursor: pointer;
   }
+
 </style>
 
-<svelte:window on:keydown={handleKeydown} />
-
-{#if Component}
-  <div
-    class="bg"
-    on:click={handleOuterClick}
-    bind:this={background}
-    transition:currentTransitionBg={state.transitionBgProps}
-    style={cssBg}>
-    <div class="window-wrap" bind:this={wrap}>
-      <div
-        class="window"
-        role="dialog"
-        aria-modal="true"
-        bind:this={modalWindow}
-        transition:currentTransitionWindow={state.transitionWindowProps}
-        on:introstart={onOpen}
-        on:outrostart={onClose}
-        on:introend={onOpened}
-        on:outroend={onClosed}
-        style={cssWindow}>
-        {#if state.closeButton}
-          {#if isSvelteComponent(state.closeButton)}
-            <svelte:component this={state.closeButton} onClose={close} />
-          {:else}
-            <button on:click={close} class="close" style={cssCloseButton}>
-              ×
-            </button>
-          {/if}
-        {/if}
-        <div class="content" style={cssContent}>
-          <svelte:component this={Component} {...props} />
-        </div>
-      </div>
+{#if open}  
+  <div on:click|self={hide} class="overlay" transition:fade="{{ duration: 200 }}">
+    <div
+      tabindex="0"
+      class:open
+      style={containerStyle}
+      on:keydown={handleEscape}
+      class="container">
+      <button on:click={hide} class="close">
+        ×
+      </button>
+      <slot />
     </div>
   </div>
 {/if}
-<slot />
