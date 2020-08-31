02c25777bc9657d3bc9291c8e1a8cdf8a0c4b226
Victoria Sloan <victoriasloan@gmail.com>
2020-08-31 22:05:26 +0100

    Label using boolean properties and redundant styles removed from svench.css

--- src/Styleguide/Label.svelte
+++ src/Styleguide/Label.svelte
@@ -1,11 +1,70 @@
 <script>
-  export let color = "dark";
-  export let size = "m";
-  export let forAttr = " ";
-
-  const classname = `bb-label bb-label--${size} bb-label--color-${color}`;
+  export let forAttr = "",
+    extraSmall = false,
+    small = false,
+    medium = false,
+    large = false,
+    extraLarge = false,
+    white = false,
+    grey = false,
+    black = false;
 </script>
 
-<label class={classname} for={forAttr}>
+<style>
+  .bb-label {
+    font-family: var(--font-sans);
+    font-weight: 500;
+    text-rendering: var(--text-render);
+    color: var(--ink);
+    font-size: var(--font-size-s);
+    margin-bottom: 12px;
+    display: block;
+    line-height: 1.5;
+  }
+
+  .extraSmall {
+    font-size: var(--font-size-xs);
+  }
+
+  .small {
+    font-size: var(--font-size-s);
+  }
+
+  .medium {
+    font-size: var(--font-size-m);
+  }
+
+  .large {
+    font-size: var(--font-size-l);
+  }
+
+  .extraLarge {
+    font-size: var(--font-size-xl);
+  }
+
+  .white {
+    color: var(--white);
+  }
+
+  .grey {
+    color: var(--grey-5);
+  }
+
+  .black {
+    color: var(--ink);
+  }
+</style>
+
+<label
+  class="bb-label"
+  class:extraSmall
+  class:small
+  class:medium
+  class:large
+  class:extraLarge
+  class:white
+  class:grey
+  class:black
+  for={forAttr}>
   <slot />
 </label>
