6e57301abcc4be1975b78711a15eaca43950c3a4
Tori Sloan <victoriasloan95@gmail.com>
2020-09-01 21:15:15 +0100

    fix: Fixing typography component inconsistencies (#110)
    * body component with boolean properties
    
    * Heading component using boolean properties
    
    * Label using boolean properties and redundant styles removed from svench.css
    
    Co-authored-by: Victoria Sloan <victoriasloan@gmail.com>

--- src/Styleguide/Heading.svelte
+++ src/Styleguide/Heading.svelte
@@ -1,10 +1,68 @@
 <script>
-  export let color = "dark";
-  export let size = "m";
-
-  const classname = `bb-heading bb-heading--${size} bb-heading--color-${color}`;
+  export let extraSmall = false,
+    small = false,
+    medium = false,
+    large = false,
+    extraLarge = false,
+    white = false,
+    grey = false,
+    black = false;
 </script>
 
-<h1 class={classname}>
+<style>
+  .bb-heading {
+    font-family: var(--font-sans);
+    font-weight: 600;
+    text-rendering: var(--text-render);
+    color: var(--ink);
+    font-size: var(--heading-font-size-m);
+    line-height: 1.5;
+  }
+
+  .extraSmall {
+    font-size: var(--heading-font-size-xs);
+  }
+
+  .small {
+    font-size: var(--heading-font-size-s);
+  }
+
+  .medium {
+    font-size: var(--heading-font-size-m);
+  }
+
+  .large {
+    font-size: var(--heading-font-size-l);
+    font-weight: 700;
+  }
+
+  .extraLarge {
+    font-size: var(--heading-font-size-xl);
+    font-weight: 700;
+  }
+
+  .white {
+    color: var(--white);
+  }
+
+  .grey {
+    color: var(--grey-5);
+  }
+
+  .black {
+    color: var(--ink);
+  }
+</style>
+
+<h1
+  class="bb-heading"
+  class:extraSmall
+  class:small
+  class:medium
+  class:large
+  class:extraLarge
+  class:white
+  class:grey
+  class:black>
   <slot />
 </h1>
