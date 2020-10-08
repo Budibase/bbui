6f3d50d7ec7917b5ccf328d377939604ec3e7d82
Andrew Kingston <aptkingston@ssl-mail.com>
2020-10-08 07:48:58 +0100

    feat: add new modal component

--- src/Modal/Modal.svelte
+++ src/Modal/Modal.svelte
@@ -1,122 +1,112 @@
 <script>
-  import { createEventDispatcher } from "svelte";
-  import buildStyle from "../utils/buildStyle";
-  import { fade } from "svelte/transition";
+  import { createEventDispatcher, setContext } from "svelte"
+  import { fade, fly } from "svelte/transition"
+  import Portal from "svelte-portal"
+  import Context from "../context";
+  const dispatch = createEventDispatcher()
 
-  const dispatch = createEventDispatcher();
-  export let maxWidth = "30vw";
-  export let maxHeight = "60vh";
-  export let minWidth = "30vw";
-  export let borderColor = "";
-  export let hideCloseButton = false;
-  export let padding="var(--layout-xs)";
+  export let padding = true
+  export let width = undefined
+  export let border = true
+  export let visible = false
 
-  export const show = () => {
-    dispatch("open");
-    open = true;
-  };
+  $: dispatch(visible ? "show" : "hide")
 
-  export const hide = () => {
-    dispatch("close");
-    open = false;
-  };
+  export function show() {
+    if (visible) {
+      return
+    }
+    visible = true
+  }
 
-  let open = null;
+  export function hide() {
+    if (!visible) {
+      return
+    }
+    visible = false
+  }
 
-  function handleEscape(e) {
-    if (open && e.key === "Escape") {
-      hide();
+  function handleKey(e) {
+    if (visible && e.key === "Escape") {
+      hide()
     }
   }
 
-  $: containerStyle = buildStyle({
-    "max-height": maxHeight,
-    "max-width": maxWidth,
-    "min-width": minWidth,
-    borderColor,
-    padding,
-  });
+  setContext(Context.Modal, { show, hide })
 </script>
+
+<svelte:window on:keydown={handleKey} />
+
+{#if visible}
+  <Portal target="body">
+    <div
+      class="overlay"
+      on:click|self={hide}
+      transition:fade={{ duration: 200 }}>
+      <div
+        class="scroll-wrapper"
+        on:click|self={hide}
+        transition:fly={{ y: 30, duration: 200 }}>
+        <div class="content-wrapper" on:click|self={hide}>
+          <div class="modal" class:padding class:border style={width ? `flex: 0 0 ${width}` : ""}>
+            <slot/>
+          </div>
+        </div>
+      </div>
+    </div>
+  </Portal>
+{/if}
 
 <style>
   .overlay {
     position: fixed;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+    left: 0;
+    right: 0;
     top: 0;
     bottom: 0;
-    right: 0;
-    left: 0;
-    z-index: 1;
-    background: rgba(0, 0, 0, 0.6);
-  }
-
-  .container {
-    outline: none;
-    box-sizing: border-box;
-    opacity: 0;
-    color: var(--ink);
-    font-weight: 400;
-    height: fit-content !important;
-    border: black;
-    border-radius: var(--border-radius-m);
+    overflow-x: hidden;
     overflow-y: auto;
-    background: #fff;
-    box-shadow: 0 5px 12px rgba(0, 0, 0, 0.15);
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    background-color: rgba(0, 0, 0, 0.25);
+    z-index: 999;
   }
 
-  .open {
-    opacity: 1;
+  .scroll-wrapper {
+    flex: 1 1 auto;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: flex-start;
+    max-height: 100%;
   }
 
-  .close {
-    display: block;
-    box-sizing: border-box;
-    position: absolute;
-    font-size: var(--font-size-xl);
-    z-index: 1000;
-    top: var(--spacing-l);
-    right: var(--spacing-l);
-    margin: 0;
-    padding: 0;
-    width: 1.5rem;
-    height: 1.5rem;
-    border: 0;
-    color: black;
-    border-radius: var(--border-radius-xl);
-    background: white;
-    transition: transform 0.2s cubic-bezier(0.25, 0.1, 0.25, 1),
-      background 0.2s cubic-bezier(0.25, 0.1, 0.25, 1);
-    -webkit-appearance: none;
-    outline: none;
+  .content-wrapper {
+    flex: 1 1 auto;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: flex-start;
+    width: 0;
   }
 
-  .close:hover {
-    background-color: var(--grey-2);
-    cursor: pointer;
+  .modal {
+    background-color: white;
+    display: grid;
+    align-items: stretch;
+    box-shadow: 0 0 4rem 1.5rem rgba(0, 0, 0, 0.15);
+    position: relative;
+    flex: 0 0 400px;
+    margin: 2rem 0;
+    border-radius: var(--border-radius-m);
+    gap: var(--spacing-xl);
   }
-
-  .close:active {
-    background-color: var(--grey-4);
-    cursor: pointer;
+  .modal.padding {
+    padding: var(--spacing-xl);
+  }
+  .modal.border {
+    border: var(--border-dark);
   }
 </style>
-
-{#if open}
-  <div on:click|self={hide} class="overlay" transition:fade={{ duration: 200 }}>
-    <div
-      tabindex="0"
-      class:open
-      style={containerStyle}
-      on:keydown={handleEscape}
-      class="container">
-      {#if !hideCloseButton}
-      <button on:click={hide} class="close">
-        ×
-      </button>
-      {/if}
-      <slot />
-    </div>
-  </div>
-{/if}
