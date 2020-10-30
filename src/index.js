472806b793cfbe20be9e2407181b5218f89c905e
Kevin Åberg Kultalahti <kevmodrome@users.noreply.github.com>
2020-10-30 09:48:05 +0100

    fix: expose RichText component (#149)

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@
 
 export { default as Input } from "./Form/Input.svelte";
 export { default as TextArea } from "./Form/TextArea.svelte";
+export { default as RichText } from "./Form/RichText.svelte";
 export { default as Select } from "./Form/Select.svelte";
 export { default as DataList } from "./Form/DataList.svelte";
 export { default as Dropzone } from "./Dropzone/Dropzone.svelte";
