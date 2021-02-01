8bff2983e760aacb1bef7b3ec4791808bd749837
Keviin Åberg Kultalahti <kevin.aberg@gmail.com>
2021-02-01 17:44:30 +0100

    progress on positioning action

--- src/actions/position_dropdown.js
+++ src/actions/position_dropdown.js
@@ -1,35 +1,70 @@
 export default function positionDropdown(element, { anchor, align }) {
-  console.log(align);
-  const { bottom, left, width } = anchor.getBoundingClientRect();
-  console.log(anchor.getBoundingClientRect());
-  const calcLeftPosition = (left) => {
-    console.log(width);
-    console.log(element.getBoundingClientRect().width);
-    return align === "right"
-      ? left + width - element.getBoundingClientRect().width
-      : left;
-  };
-  console.log(element.clientWidth);
-  element.style.position = "absolute";
-  element.style.top = `${bottom}px`;
-  element.style.minWidth = `${(width - 4).toFixed(0)}px`;
-  element.style.left = `${calcLeftPosition(left)}px`;
-  element.style.maxHeight = `${(window.innerHeight - bottom - 30).toFixed(
-    0
-  )}px`;
+  let positionSide = "top";
+  let maxHeight = 0;
+  let dimensions = getDimensions();
+  function getDimensions() {
+    const {
+      bottom,
+      top: spaceAbove,
+      left,
+      width,
+    } = anchor.getBoundingClientRect();
+    const spaceBelow = window.innerHeight - bottom;
+    const containerRect = element.getBoundingClientRect();
 
-  const resizeObserver = new ResizeObserver((entries) => {
-    for (let entry of entries) {
-      const { bottom } = entry.target.getBoundingClientRect();
-      element.style.top = `${bottom}px`;
+    let y;
+
+    if (spaceAbove > spaceBelow) {
+      positionSide = "bottom";
+      maxHeight = spaceAbove - 20;
+      y = window.innerHeight - spaceAbove;
+    } else {
+      positionSide = "top";
+      y = bottom;
+      maxHeight = spaceBelow - 20;
     }
-  });
 
-  resizeObserver.observe(anchor);
+    return {
+      [positionSide]: y,
+      left,
+      width,
+      containerWidth: containerRect.width,
+    };
+  }
 
-  return {
-    destroy() {
-      resizeObserver.disconnect();
-    },
-  };
+  function calcLeftPosition() {
+    return align === "right"
+      ? dimensions.left + dimensions.width - dimensions.containerWidth
+      : dimensions.left;
+  }
+
+  element.style.position = "absolute";
+  element.style.maxHeight = `${maxHeight.toFixed(0)}px`;
+  element.style.transformOrigin = `center ${positionSide}`;
+  element.style[positionSide] = `${dimensions[positionSide]}px`;
+  element.style.left = `${calcLeftPosition(dimensions)}px`;
+  element.style.width = dimensions.width;
+
+  //   console.log(element.clientWidth);
+  //   element.style.top = `${bottom}px`;
+  //   element.style.minWidth = `${(width - 4).toFixed(0)}px`;
+  //   element.style.left = `${calcLeftPosition(left)}px`;
+  //   element.style.maxHeight = `${(window.innerHeight - bottom - 30).toFixed(
+  //     0
+  //   )}px`;
+
+  //   const resizeObserver = new ResizeObserver((entries) => {
+  //     for (let entry of entries) {
+  //       const { bottom } = entry.target.getBoundingClientRect();
+  //       element.style.top = `${bottom}px`;
+  //     }
+  //   });
+
+  //   resizeObserver.observe(anchor);
+
+  //   return {
+  //     destroy() {
+  //       resizeObserver.disconnect();
+  //     },
+  //   };
 }
